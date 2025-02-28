b654418a64c519eaee5d9220ee1f95515968fd73
Lisa <lisa.baiden26@gmail.com>
2025-02-28 13:02:19 +0000

    second try

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// typing Animation
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "a Web Developer",
+    "an Administrator",
+    "a Freelancer",
+    "an Executive Assistant",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
